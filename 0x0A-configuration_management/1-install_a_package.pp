2480aef654a526586691bf355adb4048fb4938f1
alshimaamamdouh <alshimaa.mamdouh.abdelaziz@gmail.com>
2024-02-23 16:41:53 -0800

    task1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,4 +4,5 @@ exec { 'install_flask':
 command => '/usr/bin/pip3 install flask==2.1.0',
 path    => '/usr/local/bin:/usr/bin',
 creates => '/usr/lib/python3/dist-packages/flask',
+require => Package['python3-pip'],
 }
