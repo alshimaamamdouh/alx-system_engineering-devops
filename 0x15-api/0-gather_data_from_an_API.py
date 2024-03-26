c5d64071615050f5734010f8515bb1cc36c79c3b
alshimaamamdouh <alshimaa.mamdouh.abdelaziz@gmail.com>
2024-03-25 18:07:37 -0700

    task0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,16 +10,15 @@ if __name__ == "__main__":
     url_todo = 'https://jsonplaceholder.typicode.com/todos/'
     url_user = 'https://jsonplaceholder.typicode.com/users'
     num_tasks = 0
-    num_comp_tasks = 0;
+    num_comp_tasks = 0
     tasks = []
+
     user_info = (requests.get(url_user)).json()
     todo_info = (requests.get(url_todo)).json()
-    
 
     for i in user_info:
         if i.get('id') == int(sys.argv[1]):
             user_name = i.get('name')
-
 
     for i in todo_info:
         if i.get('userId') == int(sys.argv[1]):
@@ -28,11 +27,8 @@ if __name__ == "__main__":
                 num_comp_tasks += 1
                 tasks.append(i.get('title'))
 
-    print("Employee {} is done with tasks({}/{}):".format
-            (user_name, num_comp_tasks,num_tasks))
+    print("Employee {} is done with tasks({}/{}):".format(
+        user_name, num_comp_tasks, num_tasks))
 
     for i in tasks:
         print("\t {}".format(i))
-
-
-
